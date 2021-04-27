580f2ddf60eed428b99b00877e17dc08d7d536fa
Benjamin Shanahan <benshanahan1@gmail.com>
2021-04-26 20:45:40 -0700

    Working simple fragment shader example

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 require("file-loader?name=[name].[ext]!./src/html/index.html");
-import * as THREE from "three";
+import {
+  PerspectiveCamera,
+  Scene,
+  Mesh,
+  WebGLRenderer,
+  TorusKnotGeometry,
+  ShaderMaterial,
+  MeshNormalMaterial,
+  Vector2,
+  Clock,
+  Vector3,
+} from "three";
+import fragmentShader from "./src/glsl/frag.glsl";
 
-let camera, scene, renderer;
-let geometry, material, mesh;
+let camera;
+let scene;
+let renderer;
+let geometry;
+let material;
+let mesh;
+let clock;
+
+let u_time = 0;
+
+// uniforms are "global" variables accessible from within the shader
+let uniforms = {
+  u_resolution: { value: new Vector2(window.innerWidth, window.innerHeight) },
+  u_time: { value: u_time },
+};
 
 init();
+raf();
 
 function init() {
-  camera = new THREE.PerspectiveCamera(
+  camera = new PerspectiveCamera(
     5,
     window.innerWidth / window.innerHeight,
     0.1,
@@ -15,29 +41,52 @@ function init() {
   );
   camera.position.z = 100;
 
-  scene = new THREE.Scene();
+  scene = new Scene();
+  clock = new Clock();
 
-  geometry = new THREE.TorusKnotGeometry(1, 0.3, 100, 16);
-  material = new THREE.MeshNormalMaterial();
+  geometry = new TorusKnotGeometry(1, 0.3, 100, 16);
+  material = new ShaderMaterial({
+    uniforms,
+    fragmentShader,
+  });
 
-  mesh = new THREE.Mesh(geometry, material);
+  mesh = new Mesh(geometry, material);
   scene.add(mesh);
 
-  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer = new WebGLRenderer({ antialias: true, alpha: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setAnimationLoop(animation);
   document.body.appendChild(renderer.domElement);
 
-  // Add scroll listener.
-  window.addEventListener("wheel", function(e) {
-    camera.position.z += e.deltaY * 0.1;
-  }, true);
+  // add scroll listener
+  window.addEventListener(
+    "wheel",
+    function (e) {
+      camera.position.z += e.deltaY * 0.1;
+      if (camera.position.z < 25) camera.position.z = 25;
+      else if (camera.position.z > 200) camera.position.z = 200;
+      console.log(camera.position.z);
+    },
+    true
+  );
 }
 
-function animation(time) {
-  mesh.rotation.x = time / 2000;
-  mesh.rotation.y = time / 1000;
-  mesh.rotation.z = time / 500;
+function raf() {
+  requestAnimationFrame(raf);
+  u_time += clock.getDelta();
 
+  // update uniforms
+  material.uniforms.u_time.value = u_time;
+  material.uniforms.u_resolution.value = new Vector2(
+    window.innerWidth,
+    window.innerHeight
+  );
+
+  // rotate mesh
+  mesh.rotation.x = u_time / 2;
+  mesh.rotation.y = u_time / 1;
+  mesh.rotation.z = u_time / 0.5;
+
+  // render frame
+  renderer.clear();
   renderer.render(scene, camera);
 }
---
+++ src/glsl/frag.glsl
@@ -0,0 +1,18 @@
+uniform vec2 u_resolution;
+uniform float u_time;
+
+void main() {
+    // super simple fragment shader; rotate R, G, & B through colors at a different rate
+    vec3 colorWheel = vec3(
+        abs(sin(u_time * 0.1)),
+        abs(sin(u_time * 0.3)),
+        abs(sin(u_time * 0.5))
+    );
+    
+    // modify color by normalized (x,y) coord
+    colorWheel = colorWheel * (gl_FragCoord.x / u_resolution.x);
+    colorWheel = colorWheel * (gl_FragCoord.y / u_resolution.y);
+    
+    // set pixel color
+    gl_FragColor = vec4(colorWheel, 1.0);
+}
